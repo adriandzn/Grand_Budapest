36f2a642ed7ccc0c29803b0b23bc29e679f9a7f9
Adrian <adrianddizon17@gmail.com>
2026-05-24 01:16:46 +0800

    update index

--- index.php
+++ index.php
@@ -11,27 +11,56 @@
     
     <!-- Hero Banner -->
     <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 shadow">
+  
 
+    <!-- FIRST SECTION: Call to Action -->
+    <section class="bg-lightbrown">
+        <div class="container">
+            <div class="row py-5">
+
+                <!-- Image on the left -->
+                <div class="col-md-6 col-12 text-center d-flex flex-column justify-content-center p-5"><img src="images/placeholder1.png" alt="" class="img-fluid w-100 rounded shadow"></div>
+
+                <!-- Text on the right -->
+                <div class="col-md-6 col-12 d-flex flex-column justify-content-center font-white p-5">
+                    <div class="row">
+                        <div class="col font-title h2 fw-bold">5 STAR VINTAGE LUXURY EXPERIENCE</div>
+                    </div>
+                    <div class="row pt-2">
+                        <div class="col font-body">The Grand Budapest Hotel is a timeless icon of luxury. Experience world-class service at the most prestigious hotel in Zubrowka. From elegant rooms to exceptional amenities, every moment offers a truly unforgettable stay.</div>
+                    </div>
+
+                    <!-- Book Now Button
+                    <div class="row pt-5">
+                        <div class="col">
+                            <a href="#" class="p-5 border border-3 border-dark rounded-5 text-decoration-none font-title h5 fw-bold font-white d-flex flex-column justify-content-center text-center w-50 h-50">
+                                <div class="row">
+                                    <div class="col">BOOK NOW</div>
+                                </div>
+                                <div class="row">
+                                    <div class="col"><img src="images/logo-key-brown.png" alt="key icon" class="w-25"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> -->
+
+                </div>
+
+            </div>
+        </div>
+    </section>
     
-    <div class="row bg-lightbrown">
-        <div class="col">d</div>
-    </div>
 
-    <div class="row bg-lightpink">
-        <div class="col">d</div>
-    </div>
+    <!-- SECOND SECTION: Your Best Stay Awaits -->
 
-    <div class="row bg-lightbrown">
-        <div class="col">d</div>
-    </div>
 
-    <div class="row bg-lightpink">
-        <div class="col">d</div>
-    </div>
+    <!-- THIRD SECTION: World-Class Fine Dining -->
 
-    <div class="row bg-lightbrown">
-        <div class="col">d</div>
-    </div>
+
+    <!-- FOURTH SECTION: Relax and Unwind -->
+
+
+    <!-- FIFTH SECTION: Come Visit Us -->
 
 
     <script src="js/bootstrap.bundle.min.js"></script>
