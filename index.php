0c6f19afe184e0c45ce373f0b34ac627ff3d6fa2
Adrian <adrianddizon17@gmail.com>
2026-05-23 23:27:59 +0800

    edited css (colors and fonts)
    changed filename from color.css to body.css

--- index.php
+++ index.php
@@ -5,33 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Grand Budapest Hotel</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/colors.css">
+    <link rel="stylesheet" href="css/body.css">
 </head>
 <body>
     
     <!-- Hero Banner -->
     <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 shadow">
 
-    <div class="container">
-
-        <div class="row">
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col"></div>
-        </div>
-        <div class="row">
-            <div class="col"></div>
-        </div>
-
-    </div>
     
+    <div class="row bg-lightbrown">
+        <div class="col">d</div>
+    </div>
+
+    <div class="row bg-lightpink">
+        <div class="col">d</div>
+    </div>
+
+    <div class="row bg-lightbrown">
+        <div class="col">d</div>
+    </div>
+
+    <div class="row bg-lightpink">
+        <div class="col">d</div>
+    </div>
+
+    <div class="row bg-lightbrown">
+        <div class="col">d</div>
+    </div>
+
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
