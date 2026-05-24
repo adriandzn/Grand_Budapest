5c6e968ff26ebcae67f19406976ee5db72f363bd
Adrian <adrianddizon17@gmail.com>
2026-05-24 13:14:04 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Bar</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/body.css">
+</head>
+<body>
+    
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Left Section -->
+            <div class="col">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+
+            <!-- Logo -->
+            <div class="col">
+                <a href="">
+                    <div class="row">
+                        <div class="col">
+                            <img src="" alt="">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col"></div>
+                    </div>
+                </a>
+            </div>
+
+            <!-- Right Section -->
+            <div class="col">
+                <div class="col"></div>
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+            
+        </div>
+
+    </div>
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
