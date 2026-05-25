e738bfa5191cbdd37ba8e853d421e323751901bf
Jimweee <jandrelalberto2005@gmail.com>
2026-05-25 18:04:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -30,11 +30,11 @@
                         <div class="col font-body">The Grand Budapest Hotel is a timeless icon of luxury. Experience world-class service at the most prestigious hotel in Zubrowka. From elegant rooms to exceptional amenities, every moment offers a truly unforgettable stay.</div>
                     </div>
 
-                    <!-- Book Now Button
+                    <!-- Book Now Button -->
                     <div class="row pt-5">
                         <div class="col">
-                            <a href="#" class="p-5 border border-3 border-dark rounded-5 text-decoration-none font-title h5 fw-bold font-white d-flex flex-column justify-content-center text-center w-50 h-50">
-                                <div class="row">
+                            <a href="#" class="p-5 border bg-darkpink border-3 border-dark rounded-5 text-decoration-none font-title h5 fw-bold font-white d-flex flex-column justify-content-center text-center w-50 h-50">
+                                <div class="row text-black">
                                     <div class="col">BOOK NOW</div>
                                 </div>
                                 <div class="row">
@@ -42,7 +42,7 @@
                                 </div>
                             </a>
                         </div>
-                    </div> -->
+                    </div>
 
                 </div>
 
@@ -52,16 +52,335 @@
     
 
     <!-- SECOND SECTION: Your Best Stay Awaits -->
+    <section class="bg-lightpink py-5">
+    <div class="container">
 
+        <div class="text-center text-black mb-5">
+            <h1 class="font-title fw-bold">
+                Your Best Stay Awaits
+            </h1>
+
+            <p class="font-body">
+                Cozy yet elegant rooms, featuring a vintage charm that make every stay feel warm and refined.
+            </p>
+        </div>
+
+        <div class="row text-center g-4">
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Standard Room
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            Enjoy comfort and simplicity in our thoughtfully designed Standard Room.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Deluxe Room
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            Upgrade your stay with our Deluxe Room, featuring a more spacious layout and enhanced amenities.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Suite Room
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            Experience premium luxury in our Suite Room, designed for families or guests seeking the ultimate comfort.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
     <!-- THIRD SECTION: World-Class Fine Dining -->
+<section class="bg-lightbrown py-5">
+    <div class="container">
 
+        <div class="text-center text-white mb-5">
+            <h1 class="font-title fw-bold">
+                World-Class Fine Dining
+            </h1>
+
+            <p class="font-body">
+                Dining at the Grand Budapest Hotel is elegant yet charming, with delicious pastries, fine dishes, and attentive service.
+            </p>
+        </div>
+
+        <div class="row text-center g-4">
+
+            <div class="col-md-4">
+                <div class="bg-lightpink text-black h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Buffet
+                        </h1>
+
+                        <p class="font-body fs-6 my-4">
+                            Indulge in a lavish buffet experience featuring a curated selection of international and local cuisine.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-darkbrown fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightpink text-black h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Restaurant
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            Experience fine dining at its finest in our signature restaurant. Our chefs craft each dish with precision, using high-quality ingredients to deliver a perfect balance of flavor and presentation.
+                        </p>
+
+                       <a href="#" class="btn btn-link font-darkbrown fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightpink text-black h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Bar
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            Unwind and relax at our elegant bar, where timeless charm meets modern taste. Enjoy handcrafted cocktails, premium wines, and a wide selection of spirits in a cozy yet refined atmosphere.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-darkbrown fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
     <!-- FOURTH SECTION: Relax and Unwind -->
+<section class="bg-lightpink py-5">
+    <div class="container">
 
+        <div class="text-center text-black mb-5">
+            <h1 class="font-title fw-bold">
+                Relax and Unwind
+            </h1>
+
+            <p class="font-body">
+                The gym, pool, and spa offer a perfect place to relax and recharge, combining comfort with luxury.
+            </p>
+        </div>
+
+        <div class="row text-center g-4">
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Gym
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                           A well-equipped space designed to keep you active and energized during your stay.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                           Spa
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            A calm, relaxing space offering treatments designed to ease stress and restore balance.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="bg-lightbrown text-white h-100 shadow">
+
+                    <img src="images/placeholder1.png"
+                         class="img-fluid w-100"
+                         alt="">
+
+                    <div class="p-4">
+                        <h1 class="font-title fw-bold fs-3">
+                            Pool
+                        </h1>
+
+                        <p class="font-body fs-6 my-3">
+                            A serene swimming area perfect for a refreshing dip or quiet relaxation.
+                        </p>
+
+                        <a href="#" class="btn btn-link font-pink fs-5 text-decoration-none">
+                        Learn More
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 
     <!-- FIFTH SECTION: Come Visit Us -->
-
+<section class = "bg-lightbrown py-5">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col text-white">
+                <div class = "">
+                    <h1>
+                        Come Visit Us
+                    </h1>
+                </div>
+                <div class = "">
+                    <p>
+                        location: 1 Alpine Summit Drive Lutz, Zubrowka 1099 Republic of Zubrowka
+                    </p>
+                    <p>
+                        Contact: +63 975 714 1559
+                    </p>
+                    <p>
+                        Email: reservations@grandbudapest.zb
+                    </p>
+                </div>
+                <div class="row">
+                    <div class="col"><img src="images/logo-fb-white.png" alt="" class = "w-25 h-100"></div>
+                    <div class="col"><img src="images/logo-ig-white.png" alt="" class = "w-25 h-100"></div>
+                    <div class="col"><img src="images/logo-tiktok-white.png" alt="" class = "w-25 h-100"></div>
+                </div>
+                
+                <div class="p-5">
+                        <div class="col ">
+                            <a href="#" class="p-5 border bg-darkpink border-3 border-dark rounded-5 text-decoration-none font-title h5 fw-bold font-white d-flex flex-column justify-content-center text-center w-50 h-50">
+                                <div class="row text-black">
+                                    <div class="col">BOOK NOW</div>
+                                </div>
+                                <div class="row">
+                                    <div class="col"><img src="images/logo-key-brown.png" alt="key icon" class="w-25"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+            </div>
+            <div class="col">.
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18..."
+                    width="100%" 
+                    height="400"
+                    class="rounded shadow"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy">
+                </iframe>
+            </div>
+        </div>
+    </div>
+</section>
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
