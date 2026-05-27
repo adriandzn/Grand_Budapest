121dde0741e6810b1d1da80c7f5404d08f09662a
Jimweee <jandrelalberto2005@gmail.com>
2026-05-28 00:16:56 +0800

    Create rooms.php

---
+++ rooms.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grand Budapest Hotel</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/body.css">
+</head>
+<body>
+    
+    <!-- NAVBAR -->
+    <?php include 'navbar.php'; ?>
+
+    <!-- Hero Banner -->
+    <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 shadow">
+
+    <!-- Standard Room -->
+    <section>
+        <div class="container border rounded mt-5">
+            <div class="row">
+                <div class="col p-5">
+                    <h1 class="text-start">
+                        Standard Room
+                    </h1>
+                    <p>
+                        Enjoy comfort and simplicity in our thoughtfully designed Standard Room. Perfect for solo travelers or couples, this space offers a relaxing atmosphere with essential amenities for a pleasant stay.
+                    </p>
+                    <p>
+                        ₱4,500.00 / night
+                    </p>
+                    <p>
+                        Details:
+                        <br>
+                        2 Adults
+                        <br>
+                        1 Queen Bed
+                    </p>
+
+                    <p>
+                        Features: 
+                        <br>
+                        Free Wifi        Smart Tv
+                        <br>
+                        Air Conditioning        Private Bathroom
+                    </p>
+                </div>
+                <div class="col text-center bg-lightbrown border rounded-end  p-5">
+                    <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
+                    <br>
+                    
+                    <div class="row mt-3">
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <!-- Deluxe Room -->
+    <section>
+        <div class="container border rounded mt-5">
+            <div class="row">
+                <div class="col p-5">
+                    <h1 class="text-start">
+                        Standard Room
+                    </h1>
+                    <p>
+                        Enjoy comfort and simplicity in our thoughtfully designed Standard Room. Perfect for solo travelers or couples, this space offers a relaxing atmosphere with essential amenities for a pleasant stay.
+                    </p>
+                    <p>
+                        ₱4,500.00 / night
+                    </p>
+                    <p>
+                        Details:
+                        <br>
+                        2 Adults
+                        <br>
+                        1 Queen Bed
+                    </p>
+
+                    <p>
+                        Features: 
+                        <br>
+                        Free Wifi        Smart Tv
+                        <br>
+                        Air Conditioning        Private Bathroom
+                    </p>
+                </div>
+                <div class="col text-center bg-lightbrown border rounded-end  p-5">
+                    <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
+                    <br>
+                    
+                    <div class="row mt-3">
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+      <!-- Suite Room -->
+    <section>
+        <div class="container border rounded mt-5">
+            <div class="row">
+                <div class="col p-5">
+                    <h1 class="text-start">
+                        Standard Room
+                    </h1>
+                    <p>
+                        Enjoy comfort and simplicity in our thoughtfully designed Standard Room. Perfect for solo travelers or couples, this space offers a relaxing atmosphere with essential amenities for a pleasant stay.
+                    </p>
+                    <p>
+                        ₱4,500.00 / night
+                    </p>
+                    <p>
+                        Details:
+                        <br>
+                        2 Adults
+                        <br>
+                        1 Queen Bed
+                    </p>
+
+                    <p>
+                        Features: 
+                        <br>
+                        Free Wifi        Smart Tv
+                        <br>
+                        Air Conditioning        Private Bathroom
+                    </p>
+                </div>
+                <div class="col text-center bg-lightbrown border rounded-end  p-5">
+                    <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
+                    <br>
+                    
+                    <div class="row mt-3">
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                        <div class="col"><img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
