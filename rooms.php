d94282eaf215d6c4fac26f8b82f0ebd5fba0693b
Jimweee <jandrelalberto2005@gmail.com>
2026-05-29 10:59:25 +0800

    update

--- rooms.php
+++ rooms.php
@@ -12,8 +12,26 @@
     <!-- NAVBAR -->
     <?php include 'navbar.php'; ?>
 
-    <!-- Hero Banner -->
-    <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 shadow">
+    <!-- Hero Section -->
+
+    <section class="text-white py-5"
+        style="background-image: url('images/index-hero.png'); 
+        background-size: cover; 
+        background-position: center; 
+        background-color: rgba(0,0,0,0.6); 
+        background-blend-mode: multiply;
+        border-bottom: 0px solid #6a3e4f;">
+
+        <div class="container py-4 ps-5">
+            <div class="row">
+                <div class="col">
+                    <h1 class="display-3 font-title font-white">
+                        Rooms
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- Standard Room -->
     <section >
@@ -45,17 +63,29 @@
                     <h1 class = "fw-bold h4 mt-3">
                         Features: 
                     </h1>
+                    <div class="row">
                     <div class="col fw-bold">
-                        <img src="images/logo-profile-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        <img src="images\logo-wifi-brown.png" alt="" style = "width: 30px;" class = "me-2">
                         Free Wifi
                     </div>
                     <div class="col fw-bold">
-                        <img src="images/logo-profile-brown.png" alt="" style = "width: 30px;" class = "me-2">
-                        Air Conditioning
+                        <img src="images\logo-tv-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Smart TV
+                    </div>
+                    </div>
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-wind-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Air Conditioning 
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-toilet-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Private Bathroom
+                    </div>
                     </div>
 
                 </div>
-                <div class="col text-center bg-lightbrown border rounded-end  p-5">
+                <div class="col text-center bg-lightbrown border rounded-end p-5">
                     <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
                     <br>
                     
@@ -70,34 +100,67 @@
     </section>
 
      <!-- Deluxe Room -->
-    <section>
+    <section >
         <div class="container border rounded mt-5">
             <div class="row">
                 <div class="col p-5 bg-white">
                     <h1 class="text-start">
-                        Standard Room
+                        Deluxe Room
                     </h1>
                     <p>
-                        Enjoy comfort and simplicity in our thoughtfully designed Standard Room. Perfect for solo travelers or couples, this space offers a relaxing atmosphere with essential amenities for a pleasant stay.
+                        Upgrade your stay with our Deluxe Room, featuring a more spacious layout and enhanced amenities. Ideal for guests who want both comfort and a touch of luxury.
                     </p>
-                    <p>
-                        ₱4,500.00 / night
+                    <p class = "fw-bold font-pink ">
+                        ₱8,599.00 / night
                     </p>
-                    <p>
+                    
+                    <h1 class = "fw-bold h4">
                         Details:
-                        <br>
-                        2 Adults
-                        <br>
-                        1 Queen Bed
-                    </p>
+                    </h1>
+                    <div class="col fw-bold">
+                        <img src="images/logo-profile-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        2 Adults • 2 Child
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images/logo-bed-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        1 King Bed and 2 Twin Beds
+                    </div>
 
-                    <p>
+                    <h1 class = "fw-bold h4 mt-3">
                         Features: 
-                        <br>
-                        Free Wifi        Smart Tv
-                        <br>
-                        Air Conditioning        Private Bathroom
-                    </p>
+                    </h1>
+
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-wifi-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Free Wifi
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-toilet-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Private Bathroom
+                    </div>
+                    </div>
+
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-wind-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Air Conditioning 
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-sun-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Balcony
+                    </div>
+                    </div>
+
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-tv-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Smart TV
+                    </div>
+                    </div>
+
+
+
                 </div>
                 <div class="col text-center bg-lightbrown border rounded-end  p-5">
                     <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
@@ -112,35 +175,71 @@
             </div>
         </div>
     </section>
-      <!-- Suite Room -->
-    <section>
+      
+    <!-- Suite Room -->
+    <section >
         <div class="container border rounded mt-5">
             <div class="row">
                 <div class="col p-5 bg-white">
                     <h1 class="text-start">
-                        Standard Room
+                        Suite Room
                     </h1>
                     <p>
-                        Enjoy comfort and simplicity in our thoughtfully designed Standard Room. Perfect for solo travelers or couples, this space offers a relaxing atmosphere with essential amenities for a pleasant stay.
+                        Experience premium luxury in our Suite Room, designed for families or guests seeking the ultimate comfort. With elegant interiors and generous space, this room ensures a truly memorable stay.
                     </p>
-                    <p>
-                        ₱4,500.00 / night
+                    <p class = "fw-bold font-pink ">
+                        ₱14,999.00 / night
                     </p>
-                    <p>
+                    
+                    <h1 class = "fw-bold h4">
                         Details:
-                        <br>
-                        2 Adults
-                        <br>
-                        1 Queen Bed
-                    </p>
+                    </h1>
+                    <div class="col fw-bold">
+                        <img src="images/logo-profile-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        4 Adults • 4 Child
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images/logo-bed-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        2 King Beds and 2 Twin Beds
+                    </div>
 
-                    <p>
+                    <h1 class = "fw-bold h4 mt-3">
                         Features: 
-                        <br>
-                        Free Wifi Smart Tv
-                        <br>
-                        Air Conditioning        Private Bathroom
-                    </p>
+                    </h1>
+
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-wifi-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Free Wifi
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-wine-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Mini Bar
+                    </div>
+                    </div>
+                    
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-wind-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Air Conditioning 
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-toilet-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Private Bathroom
+                    </div>
+                    </div>
+
+                    <div class="row">
+                    <div class="col fw-bold">
+                        <img src="images\logo-tv-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Smart TV
+                    </div>
+                    <div class="col fw-bold">
+                        <img src="images\logo-sun-brown.png" alt="" style = "width: 30px;" class = "me-2">
+                        Balcony
+                    </div>
+                    </div>
+
                 </div>
                 <div class="col text-center bg-lightbrown border rounded-end  p-5">
                     <img src="images/index-hero.png" alt="hero banner" class="img-fluid w-100 rounded shadow">
@@ -155,7 +254,61 @@
             </div>
         </div>
     </section>
-   
+    
+    <!-- CTA Section -->
+
+    <section class="my-5">
+
+        <div class="container">
+
+            <div class="row align-items-center rounded-4 px-5 py-4 bg-lightbrown">
+
+                <!-- Logo -->
+
+                <div class="col-lg-2 text-center text-lg-start mb-4 mb-lg-0">
+
+                    <img src="images/logo.png"
+                        alt="Grand Budapest Logo"
+                        class="img-fluid"
+                        style="max-height: 90px;">
+
+                </div>
+
+                <!-- Text -->
+
+                <div class="col-lg-7 text-center text-lg-start mb-4 mb-lg-0">
+
+                    <h2 class="h2 text-white fw-bold mb-3">
+                        Ready To Experience Timeless Elegance?
+                    </h2>
+
+                    <p class="text-light small mb-0">
+                        Book now and enjoy curated activities, signature treatments,
+                        and premium amenities in a beautifully restored grand hotel setting.
+                    </p>
+
+                </div>
+
+                <!-- Button -->
+
+                <div class="col-lg-3 text-center text-lg-end">
+
+                    <a href="#"
+                        class="btn rounded-pill px-5 py-3 bg-lightpink font-darkbrown fw-semibold border border-darkpink">
+
+                        Book Now
+
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+   <?php include 'footer.php'; ?>
 
     <script src="js/bootstrap.bundle.min.js"></script>
 
