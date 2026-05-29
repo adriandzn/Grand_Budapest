151656e3127e3152af632e81dd37c56786f71277
Adrian <adrianddizon17@gmail.com>
2026-05-29 11:33:59 +0800

    updates

--- book_1.php
+++ book_1.php
@@ -27,9 +27,70 @@
                 </div>
             </div>
         </div>
-
     </section>
 
+
+
+
+
+    <div class="container">
+
+        <!-- PROGRESS BAR -->
+        <!-- <?php include 'progress.php'; ?> -->
+
+        <!-- BOOKING CARD -->
+        <div class="booking-card shadow-lg">
+            <h1 class="fw-bold mb-5 text-white">
+                Choose Your Dates
+            </h1>
+            <div class="row g-4 align-items-end">
+
+                <!-- CHECK IN -->
+                <div class="col-lg-4">
+                    <label class="form-label text-white fw-bold">
+                        Check-in
+                    </label>
+                    <input type="date" class="form-control custom-input">
+                </div>
+
+                <!-- CHECK OUT -->
+                <div class="col-lg-4">
+                    <label class="form-label text-white fw-bold">
+                        Check-out
+                    </label>
+                    <input type="date" class="form-control custom-input">
+                </div>
+
+                <!-- NIGHTS -->
+                <div class="col-lg-4">
+                    <div class="nights-box text-center">
+                        6 nights
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- NOTE -->
+            <div class="mt-5 text-white">
+                <p class="fw-bold mb-1">
+                    Note:
+                </p>
+                <p>
+                    Check-in: 02:00 PM |
+                    Check-out: 12:00 PM
+                </p>
+            </div>
+
+            <!-- BUTTON -->
+            <div class="text-center mt-5">
+                <button class="btn next-btn px-5 py-2">
+                    Next
+                </button>
+            </div>
+
+        </div>
+
+    </div>
 
 
 
