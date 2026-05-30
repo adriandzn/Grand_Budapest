3704d47180c2f74ec2c5aa2a5ee6053462a5413d
Adrian <adrianddizon17@gmail.com>
2026-05-30 11:56:15 +0800

    changed login

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -20,17 +25,25 @@
             color: #FFC093;
             font-weight: bold;
         }
+        .register {
+            color: white;
+            transition: color 0.3s ease;
+        }
+        .register:hover {
+            color: #afafaf;
+        }
     </style>
 </head>
-<body  style="background-image: url('images/index-hero.png'); 
-             background-size: cover; 
-             background-position: center; 
-             background-color: rgba(10, 10, 10, 0.75); 
-             background-blend-mode: multiply; 
-             min-height: 100vh; 
-             background-attachment: fixed;
-             border-bottom: 0px solid #6a3e4f;
-             backdrop-filter: blur(5px);">
+
+<body style="background-image: url('images/index-hero.png'); 
+    background-size: cover; 
+    background-position: center; 
+    background-color: rgba(10, 10, 10, 0.75); 
+    background-blend-mode: multiply; 
+    min-height: 100vh; 
+    background-attachment: fixed;
+    border-bottom: 0px solid #6a3e4f;
+    backdrop-filter: blur(5px);">
 
     <div class="container min-vh-100 d-flex align-items-center justify-content-center py-5">
         <div class="row justify-content-center w-100">      
@@ -59,8 +72,10 @@
                         </div>
                     </div>
 
+
                     <!-- FORM -->
                     <form action="login.php" method="post">
+
                         <!-- USERNAME -->
                         <div class="form-outline mb-4">
                             <label
@@ -93,7 +108,6 @@
                         </div>
 
                         <!-- LOGIN BUTTON -->
-
                         <div class="row justify-content-center">
                             <div class="col-12 col-md-8 text-center">
                                 <input
@@ -105,19 +119,20 @@
                         </div>
 
                         
-
                         <!-- REGISTER -->
-                        <div class="row text-center text-white font-body">
+                        <div class="row text-center text-white">
                             <div class="col fs-5">
-                                Don't have an account?
-                                <br>
+                                <div class="my-3 font-title">Don't have an account?</div>
                                 <a href="register.php"
-                                    class="btn btn-link text-white fs-5 text-decoration-none fw-bold">
+                                    class="font-body text-decoration-none fw-bold register">
                                     Register Here
                                 </a>
                             </div>
                         </div>
+
                     </form>
+
+
                 </div>
             </div>
         </div>
@@ -126,89 +141,70 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </body>
-
 </html>
+
+
 <?php
-require_once "dbaseconnection.php";
+    require_once "dbaseconnection.php";
 
 
-session_start();
+    if(isset($_POST['sub'])){
+
+        $GBusername = $_POST['username'];
+        $GBpassword = md5($_POST['pass']);
+
+        $loginsql = "SELECT * FROM tbl_userdetails WHERE username = '$GBusername' AND password = '$GBpassword' AND status = 'Active'";
+        $result = $conn -> query($loginsql);
 
 
+        // VALID Log In Credentials
+        if($result -> num_rows == 1) {
+            $fieldnames = $result -> fetch_assoc();
+
+            $GBid = $fieldnames['user_id'];
+            $GBfullname = $fieldnames['full_name'];
+            $GBrole = $fieldnames['role'];
+            $GBusername = $fieldnames['username'];
+            $GBemail = $fieldnames['email'];
 
 
+            // SESSION VARIABLES
+            $_SESSION['GBid'] = $GBid;
+            $_SESSION['GBfullname'] = $GBfullname;
+            $_SESSION['GBrole'] = $GBrole;
+            $_SESSION['GBusername'] = $GBusername;
+            $_SESSION['GBemail'] = $GBemail;
+            
+
+            // LOGS - Logging In
+            $logsql = "INSERT INTO tbl_logs (user_id, action, date_time) VALUES ('" . $_SESSION['GBid'] . "', 'Logged In', NOW())";
+            $conn -> query($logsql);
 
 
+            // Identify User ROLE and direct to corresponding page
+            if ($GBrole == "Admin") {
+                header("location:admin_dashboard.php");
+            } else if ($GBrole == "Employee") {
+                header("location:employee_dashboard.php");
+            } else if ($GBrole == "Customer") {
+                header("location:index.php");
+            }
 
 
-if(isset($_POST['sub'])){
-
-
- $JAusername = $_POST['username'];
- $JApassword = md5($_POST['pass']);
-
-$loginsql = "SELECT * from tbl_userdetails_ja WHERE username = '$JAusername' AND pass = '$JApassword' AND statusja = 'Active'";
-$result = $conn -> query($loginsql);
-
-if($result->num_rows ==1){
-$fieldnames = $result->fetch_assoc();
-
-$JAusertype = $fieldnames['user_typeja'];
-$JAlastname = $fieldnames['last_name'];
-$JAfirstname = $fieldnames['first_name'];
-$JAimage_path = $fieldnames['img_pathja'];
-$JAid = $fieldnames['user_id'];
-
-//session variables = value
-        $_SESSION['JAusertype'] = $JAusertype;
-        $_SESSION['JAfullname'] = $JAfirstname . " " . $JAlastname;
-        $_SESSION['JAimagepath'] = $JAimage_path;
-        $_SESSION['JAid'] = $JAid;
-
-
-        $logsql = "Insert into tbl_logsja (user_id, action, datetime) VALUES ('".$_SESSION['JAid']."', 'Logged In',NOW())";
-        $conn -> query($logsql);
-
-
-
-
-        echo $JAusertype . " " . $JAlastname;
-
-
-
-
-
-
-
-
-        if ($JAusertype == "Administrator"){
-            header("location:admindashboard.php");
+        // INVALID Log In Credentials
+        } else {
+            ?>
+            <script>
+                Swal.fire({
+                    position: "center",
+                    icon: "error",
+                    title: "Invalid Username/Password/Not Verified",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            </script>
+            <?php
         }
-        else if($JAusertype == "Employee"){
-        ?>
-        <script>
-            window.location.href = "employeedashboard.php";
-        </script>
-        <?php
-        }
-
-
-
-
     }
-    else{
 
- ?>
-        <script>
-            Swal.fire({
-                position: "center",
-                icon: "error",
-                title: "Invalid Username/Password",
-                showConfirmButton: false,
-                timer: 1500
-            });
-        </script>
-        <?php
-    }
-}
 ?>
